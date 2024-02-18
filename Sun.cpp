c74821231c377a35fb33405ad61a034638699d27
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2024-02-18 19:34:00 +0300

    Update Sun.cpp

--- Sun.cpp
+++ Sun.cpp
@@ -10,10 +10,6 @@ bool Sun::is_in_gold_zone(double distance) {
 }
 
 Sun::Sun() : Star("main_sequence", 3.828 * pow(10, 26), 695500, 2 * pow(10, 30)) {
-    this->radius = 695500;
-    this->massa = 2 * pow(10, 30);
     this->gold_zone = 1.496 * pow(10, 8);
-    this->luminosity = 3.828 * pow(10, 26);
-    this->type = "main sequence";
     this->planets_around = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uran", "Neptune"};
 }
