1838a1ab5f9640f324ebe31d507feb6c04261393
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2024-02-26 12:43:57 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "SpaceObject.h"
+#include "Star.h"
+#include "Comet.h"
+#include "BlackHole.h"
+#include "Sun.h"
+#include "Sirius.h"
+
+
+int main() {
+    Star star("blue giant", 1, 1, 1);
+    Sun our_sun;
+    return 0;
+}
