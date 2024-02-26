27f233e8ba36fc1bfda374ecff5785dc80653e15
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2024-02-26 12:44:15 +0300

    Update BlackHole.cpp

--- BlackHole.cpp
+++ BlackHole.cpp
@@ -11,4 +11,5 @@ BlackHole::BlackHole(double distance) : Star("black hole",  0, 0, 0) {
 
 double BlackHole::count_and_get_radius() {
     this->radius = sqrt(2 * 6.67 * pow(10, -11) * this->massa / pow((3 * pow(10, 8)), 2));
+    return this->radius;
 }
